8c769b52fa8a5a8936aff34fd8b2d51f264fb816
Matthies Haentjens <haentjens.matthies@gmail.com>
2020-11-03 10:50:51 +0700

    Back to starter code restructured into separate static components
    Diving immediately into separate components, calling the API and working
    with state and props got me a bit lost on where state should live.
    So I set everything back to how it was and I am following the example from
    https://reactjs.org/docs/thinking-in-react.html building it steps by step
    from a static app without API calls and state to a dynamic one with API calls
    and state

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,13 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import * as BooksAPI from '../BooksAPI'
 import { Book } from "./Book";
 import { arrayContent } from "../helpers"
 
 class SearchBooks extends React.Component {
 
-    static propTypes = {
-        books: PropTypes.array
-    }
+    // static propTypes = {
+    //     books: PropTypes.array
+    // }
+
+    // state = {
+    //     books: {}
+    // }
+
+    // updateQuery = (query) => {
+    //     BooksAPI.search(query).then((books) => {
+    //         this.setState(() => ({
+    //             books,
+    //         }));
+    //     });
+    // }
 
     render() {
 
@@ -16,21 +29,25 @@ class SearchBooks extends React.Component {
             <div className="search-books-bar">
             {/* <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button> */}
             <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author" />
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author" 
+                    // onChange={(event) => this.updateQuery(event.target.value)}
+                />
             </div>
             </div>
             <div className="search-books-results">
-            <ol className="books-grid">
-                    {this.props.books.map((book) => (
-                        <li key={book.id}>
-                            <Book 
-                                title={book.title}
-                                authors={arrayContent(book.authors)}
-                                thumbnail={book.imageLinks.smallThumbnail}
-                            />
-                        </li>)
-                    )}
-            </ol>
+                <ol className="books-grid">
+                        {/* {this.state.books.map((book) => (
+                            <li key={book.id}>
+                                <Book 
+                                    title={book.title}
+                                    authors={arrayContent(book.authors)}
+                                    thumbnail={book.imageLinks.smallThumbnail}
+                                />
+                            </li>)
+                        )} */}
+                </ol>
             </div>
         </div>
         );
