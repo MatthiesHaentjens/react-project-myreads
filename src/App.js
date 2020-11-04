70c56820c0fbe77c316e8e99b90145b757e8d630
Matthies Haentjens <haentjens.matthies@gmail.com>
2020-11-04 09:25:28 +0700

    Renders shelf and books component with only props no state yet
    Build a bookshelf and book component that take in values from a
    dummy database (not the API yet) and based on the contents of
    the database render the number of applicable components.
    Next step is assigning the books to a shelf

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class BookShelfChanger extends React.Component {
 }
 
 class Book extends React.Component {
-
   render() {
     return (
       <div className="book">
@@ -42,14 +41,13 @@ class Book extends React.Component {
             style={{
               width: 128,
               height: 188,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
+              backgroundImage: `url(${this.props.thumbnail})`,
             }}
           />
           <BookShelfChanger />
         </div>
-        <div className="book-title">Ender's Game</div>
-        <div className="book-authors">Orson Scott Card</div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
       </div>
     );
   }
@@ -59,13 +57,15 @@ class BookShelfRow extends React.Component {
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{this.props.shelf}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
+            {books.map((book, index) => (
+            <li key={index}>
+              <Book title={book.title} authors={book.authors} thumbnail={book.thumbnail}/>
             </li>
-            <li>
+            ))}
+            {/* <li>
               <div className="book">
                 <div className="book-top">
                   <div
@@ -94,7 +94,7 @@ class BookShelfRow extends React.Component {
                 <div className="book-title">Ender's Game</div>
                 <div className="book-authors">Orson Scott Card</div>
               </div>
-            </li>
+            </li> */}
           </ol>
         </div>
       </div>
@@ -111,8 +111,10 @@ class BooksShelfTable extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelfRow />
-            <div className="bookshelf">
+            {shelves.map((shelf, index) => (
+            <BookShelfRow key={index} shelf={shelf.name} books={this.props.books}/>
+            ))}
+            {/* <div className="bookshelf">
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
@@ -281,7 +283,7 @@ class BooksShelfTable extends React.Component {
                   </li>
                 </ol>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <OpenSearch />
@@ -295,7 +297,12 @@ class SearchBooks extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <button
+            className="close-search"
+            onClick={() => this.setState({ showSearchPage: false })}
+          >
+            Close
+          </button>
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author" />
           </div>
@@ -322,10 +329,63 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? <SearchBooks /> : <BooksShelfTable />}
+        {this.state.showSearchPage ? (
+          <SearchBooks />
+        ) : (
+          <BooksShelfTable shelves={this.props.shelves} />
+        )}
       </div>
     );
   }
 }
+
+const books = [
+  {
+    id: 1,
+    title: "The Adventures of Tom Sawyer",
+    authors: "Mark Twain",
+    thumbnail:
+      "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
+    shelf: "Currently Reading",
+  },
+  {
+    id: 2,
+    title: "1776",
+    authors: "David McCullough",
+    thumbnail:
+      "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+    shelf: "Currently Reading",
+  },
+  {
+    id: 3,
+    title: "Harry Potter and the Sorcerer's Stone",
+    authors: "J.K. Rowling",
+    thumbnail:
+      "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+    shelf: "Read",
+  },
+  {
+    id: 4,
+    title: "The Hobbit",
+    authors: "J.R.R. Tolkien",
+    thumbnail:
+      "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
+    shelf: "Read",
+  },
+  {
+    id: 5,
+    title: "Oh, the Places You'll Go!",
+    authors: "Seuss",
+    thumbnail:
+      "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
+    shelf: "Want to Read",
+  },
+];
+
+const shelves = [
+  { name: "Currently Reading" },
+  { name: "Want to Read" },
+  { name: "Read" },
+];
 
 export default BooksApp;
